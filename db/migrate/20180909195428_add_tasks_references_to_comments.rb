3c8db80b797c2b2a240bdba6c3edf1440a5ad94d
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-09-09 21:56:34 +0200

    adds comments task references to comments

---
+++ db/migrate/20180909195428_add_tasks_references_to_comments.rb
@@ -0,0 +1,5 @@
+class AddTasksReferencesToComments < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :comments, :task, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_09_134605) do
+ActiveRecord::Schema.define(version: 2018_09_09_195428) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.integer "user_id"
-    t.integer "task_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "task_id"
     t.index ["task_id"], name: "index_comments_on_task_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
