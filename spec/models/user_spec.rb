dec1069e5bd4a4223b04ba5ff9a75ae11acfc96b
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-09-09 15:44:59 +0200

    adds guard configurations for TDD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ActiveModel validations' do
-    it { should }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
   end
   describe 'AcriveRecord Associations' do
   end
