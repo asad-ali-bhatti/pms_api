709136a4556ed5ba94531e0e05c66d313e5173de
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-09-09 21:53:44 +0200

    reverted migrational changes

--- db/migrate/20180909123549_create_tasks.rb
+++ db/migrate/20180909123549_create_tasks.rb
@@ -3,9 +3,8 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :title
       t.text :description
-      # t.references :project, foreign_key: true
-      t.references :tasks, foreign_key: true
-      
+      t.references :project, foreign_key: true
+
       t.timestamps
     end
   end
