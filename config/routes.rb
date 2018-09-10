5fa48b475ed4ac3f0ce9663fa16334c5d5d5f5ac
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-09-10 16:50:28 +0200

    task as nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :comments
       resources :users
-      resources :tasks
-      resources :projects
+      resources :projects do
+        resources :tasks
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
